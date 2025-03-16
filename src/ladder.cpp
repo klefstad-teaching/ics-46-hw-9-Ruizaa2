72fb56f5170c8e8bc58c492e4ba7ff9ee88026b5
Ruizaa2 <ruizaa2@uci.edu>
2025-03-16 15:11:58 +0000

    verify word ladder from hw objectives

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -83,8 +83,6 @@ vector<string> generate_word_ladder(const string &begin_word, const string &end_
         return {};
     }
 
-    
-
     queue<vector<string>> ladderQueue;
     ladderQueue.push({begin_word});
 
@@ -113,6 +111,7 @@ vector<string> generate_word_ladder(const string &begin_word, const string &end_
     }
     return {};
 }
+
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d){(void)str1;(void)str2; (void)d; return false;};
 
 
@@ -127,5 +126,24 @@ void print_word_ladder(const vector<string>& ladder)
     cout << endl;
 }
 
+#define my_assert(e) {cout << #e << ((e) ? " passed": " failed") << endl;}
 
-void verify_word_ladder(){};
+void verify_word_ladder() {
+
+    set<string> word_list;
+
+    load_words(word_list, "words.txt");
+
+    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+
+    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+
+    my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
+
+    my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
+
+    my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+
+    my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
+
+}
